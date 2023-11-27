c38ab45d0695dcd15bb12d156900d8add7ce2cd1
chou0122 <74644793+chou0122@users.noreply.github.com>
2023-11-27 16:17:00 +0800

    .

--- 11,27.jsx
+++ 11,27.jsx
@@ -5,7 +5,7 @@
 // const a=[1,2,3]
 // const b=a[2]
 // a[2]=99 //給2的位置設定叫做99的新職
-// const[x,y,z]=a //解構賦值語法 //類似鏡子1對1應對做指定運算，把陣中的資料擷取成為獨立變數 //執行後結果為`x=1,y=2,z=3`
+// const[x,y,z]=a //解構賦值語法 //類似鏡子1對1應對做指定運算，把矩陣中的資料擷取成為獨立變數 //執行後結果為`x=1,y=2,z=3`
 
 // const a=[1,2,3,4]
 // for(leti=0;i<blur.length;i++){
@@ -18,3 +18,13 @@
 const Email=[@mail.com]
 <lable className="1">test2</lable>
 </> */
+
+//use state範例
+// import{useState}from'react'
+// export default function Counter(){
+//     const[count,setCount]=useState(0)  //宣告state狀態
+//     const updateCount=()=>{
+//         setCount(count+1)
+//     }
+// }
+
