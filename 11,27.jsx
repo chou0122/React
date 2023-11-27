deeabef939ab0193dbe9a44713d85ec3f59223aa
chou0122 <dannychou0122@gmail.com>
2023-11-27 19:35:34 +0800

    Update 11,27.jsx

--- 11,27.jsx
+++ 11,27.jsx
@@ -1,30 +1,30 @@
-// const a=[1,2,3]
-// let popValue=a.pop() //從後面移除
-// let shiftValue=a.shift() //從前面移除
+const a=[1,2,3]
+let popValue=a.pop() //從後面移除
+let shiftValue=a.shift() //從前面移除
 
-// const a=[1,2,3]
-// const b=a[2]
-// a[2]=99 //給2的位置設定叫做99的新職
-// const[x,y,z]=a //解構賦值語法 //類似鏡子1對1應對做指定運算，把矩陣中的資料擷取成為獨立變數 //執行後結果為`x=1,y=2,z=3`
 
-// const a=[1,2,3,4]
-// for(leti=0;i<blur.length;i++){
-//     b[i]=b[i]*2
-// }
+const b=a[2]
+a[2]=99 //給2的位置設定叫做99的新職
+const[x,y,z]=a //解構賦值語法 //類似鏡子1對1應對做指定運算，把矩陣中的資料擷取成為獨立變數 //執行後結果為`x=1,y=2,z=3`
 
-/* <>
+const a=[1,2,3,4]
+for(leti=0;i<blur.length;i++){
+    b[i]=b[i]*2
+}
+
+<>
 <h1 class="tittle">text</h1>
 <label htmlFor="emailinput">Email</label>
 const Email=[@mail.com]
 <lable className="1">test2</lable>
-</> */
+</> 
 
 //use state範例
-// import{useState}from'react'
-// export default function Counter(){
-//     const[count,setCount]=useState(0)  //宣告state狀態
-//     const updateCount=()=>{
-//         setCount(count+1)
-//     }
-// }
+import{useState}from'react'
+export default function Counter(){
+    const[count,setCount]=useState(0)  //宣告state狀態
+    const updateCount=()=>{
+        setCount(count+1)
+    }
+}
 
