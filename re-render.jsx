a7e4fc87b08ad0fa159201dc18041259751c47ec
chou0122 <74644793+chou0122@users.noreply.github.com>
2023-11-30 08:49:42 +0800

    Update re-render.jsx

--- re-render.jsx
+++ re-render.jsx
@@ -1,8 +1,19 @@
-import React from 'react'
+import { useState } from 'react'
 
-export default function CRender() {
+export default function ReRender() {
+  const [total, setTotal] = useState(0)
+  console.log('render');
+  return (
+    <>
+      <h1>re-render範例</h1>
+      <h1>{total}</h1>
+      <button className='test' onClick={() => {
+        setTotal(total + 1)
+      }}>+1</button>
+    </>
+  )
   const count = 0
-    return <>{count && <h1>Messages:{count}</h1>}</>
-    return<>{count ? <h1>Messages:{count}</h1>:''}</>
-  
+  return <>{count && <h1>Messages:{count}</h1>}</>
+  return <>{count ? <h1>Messages:{count}</h1> : ''}</>
+
 }
