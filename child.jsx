b7cc78570af0b594dfcd2b82b267df3696e0c8a3
chou0122 <74644793+chou0122@users.noreply.github.com>
2023-12-05 12:00:19 +0800

    .

--- child.jsx
+++ child.jsx
@@ -1,11 +1,17 @@
 import React from 'react'
 import props from './props';
+import Parent from './parent';
 
-export default function Child({ text, price, hasStock, sum, aa, odjA }) {
+export default function Child(props) {
     console.log(props)
+    const[cData,setData]=useState('data')
+    props.setDataFromChild(cData)
     return (
         <>
             <h2>子女元件(child)</h2>
+            <button onClick={()=>{
+                props.setDataFromChild(cData)
+            }}>回傳資料給父母</button>
             <p>text: {props.text}</p>
             <p>price: {props.price}</p>
             <p>hasStock: {props.hasStock ? 'true' : 'false'}</p>
--- parent.jsx
+++ parent.jsx
@@ -1,8 +1,15 @@
 import React from 'react'
 import Child from './child'
+import { useEffect } from 'react'
 
 export default function Parent() {
   return (
+    // useEffect(() => {
+    //   effect
+    //   return () => {
+    //     cleanup
+    //   };
+    // }, [input]);
     <>
         <h1>父母元件(Parent)</h1>
     <Child text="今天開始躺平學程式" price={9999} hasStock={false}/>
